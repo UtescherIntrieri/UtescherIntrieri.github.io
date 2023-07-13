23614e27405d5d5bc1525032ba5c3e10c8700217
UtescherIntrieri <18ianaui1999@gmail.com>
2023-07-13 20:57:26 -0300

    .

--- index.js
+++ index.js
@@ -86,14 +86,14 @@ document.addEventListener("DOMContentLoaded", function() {
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
 
-/* -- Cards effect -- */
-document.getElementById("cards").onmousemove = e => {
-  for(const card of document.getElementsByClassName("card")) {
-    const rect = card.getBoundingClientRect(),
-    x = e.clientX - rect.left,
-    y = e.clientY - rect.top;
+// /* -- Cards effect -- */
+// document.getElementById("cards").onmousemove = e => {
+//   for(const card of document.getElementsByClassName("card")) {
+//     const rect = card.getBoundingClientRect(),
+//     x = e.clientX - rect.left,
+//     y = e.clientY - rect.top;
     
-    card.style.setProperty("--mouse-x", `${x}px`);
-    card.style.setProperty("--mouse-y", `${y}px`);
-  };
-}
+//     card.style.setProperty("--mouse-x", `${x}px`);
+//     card.style.setProperty("--mouse-y", `${y}px`);
+//   };
+// }
